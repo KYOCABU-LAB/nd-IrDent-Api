01d8c1a19c274973e9ba61734ee017adcb90d226
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-11 18:55:48 -0500

    update schemas

--- prisma/migrations/20250911235515_add_schema_dabase/migration.sql
+++ prisma/migrations/20250911235515_add_schema_dabase/migration.sql
@@ -1,11 +1,12 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `TestUser` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
 
-  - You are about to drop the `test_users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `test_users`;
+    UNIQUE INDEX `TestUser_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `User` (
