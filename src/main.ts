bff511950034b3199dc116af5e8de3384e616981
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-14 18:18:56 -0500

    feat: add global validation pipe with whitelist and transform options
    Enable input validation and transformation by adding a global ValidationPipe
    with whitelist, forbidNonWhitelisted and transform options to enhance API security

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './shared/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
 
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
